660da9aab7d76e687ea8b55a70953adfc1ed57ba
Davi P. Oliveira <dpo.dev11@gmail.com>
2025-10-13 12:33:33 -0300

    added typecasting.py

---
+++ 03-Typecasting/typecasting.py
@@ -0,0 +1,23 @@
+#Typecasting = the process of converting a variable from one data type to another
+#               str(), int(), float(), bool()
+
+name = "aaaaaa"
+age = 25
+gpa = 3.2
+is_student = True
+
+# print(type(age));
+
+gpa = int(gpa)
+
+print(gpa)
+
+age = str(age)
+
+age += "1";
+
+print(type(age))
+
+name = bool(name)
+
+print(name)
